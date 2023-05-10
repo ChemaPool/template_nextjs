040d7bcc7cd2325b3654ebc944282d251d410a4b
ChemaPool <jose.pool96@outlook.com>
2023-05-10 10:40:33 -0600

    [TNJS-Fix] - Add Base Url

--- src/pages/api/logout-google/index.ts
+++ src/pages/api/logout-google/index.ts
@@ -1,9 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  // console.log("cierro sesi;on de gugul");
   res.redirect(
-    "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost:3000/"
+    `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=${process.env.NEXT_PUBLIC_BASE_URL}`
   );
 };
 
